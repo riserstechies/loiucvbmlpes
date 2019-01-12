2d92c978563f222e7ad08c5dd4d6218e5f00ee9c
riserstechies <46120655+riserstechies@users.noreply.github.com>
2019-01-13 00:27:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="en-US">
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="refresh" content="1; url=https://www.youtube.com/user/IanH/videos">   
+        <meta http-equiv="refresh" content="1; url=https://www.amazon.com/OutdoorMaster-OTG-Ski-Goggles-Protection/dp/B01HLV5JLU/ref=gbps_tit_m-9_53c0_67577861?smid=A1X43XAF7536UH&pf_rd_p=806222ac-2f87-4ede-a31c-ac60f36f53c0&pf_rd_s=merchandised-search-9&pf_rd_t=101&pf_rd_i=15529609011&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=HP1A97N09CW6XEQYCGNC">   
         <script type="text/javascript">
-               window.location.href = "https://www.youtube.com/user/IanH/videos"
+               window.location.href = "https://www.amazon.com/OutdoorMaster-OTG-Ski-Goggles-Protection/dp/B01HLV5JLU/ref=gbps_tit_m-9_53c0_67577861?smid=A1X43XAF7536UH&pf_rd_p=806222ac-2f87-4ede-a31c-ac60f36f53c0&pf_rd_s=merchandised-search-9&pf_rd_t=101&pf_rd_i=15529609011&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=HP1A97N09CW6XEQYCGNC"
         </script>
         <title>Page Redirection</title>
     </head>
